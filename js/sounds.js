5549d80439b311995221d34d1b177f4af1e144fc
Jared <jared.glastetter@gmail.com>
2018-08-13 22:15:55 -0500

    version sound files

---
+++ js/sounds.js
@@ -0,0 +1,20 @@
+//Sounds file
+
+/*    UI Sound Effects  */
+
+// var IconBarClick_Sound = new Audio("sounds/button_click.mp3");
+// var IconBarHover_Sound = new Audio("sounds/button_hover.mp3");
+
+// var TabContentClick_sound = new Audio("sounds/button_click.mp3");
+// var TabContentHover_Sound = new Audio("sounds/button_hover.mp3");
+
+var Button_Click_Sound = new Audio();//"sounds/button_click.mp3");
+var Button_Hover_Sound = new Audio();//"sounds/button_hover.mp3");
+Button_Hover_Sound.src = "sounds/button_hover.mp3";
+Button_Click_Sound.src = "sounds/button_click.mp3";
+
+var Opening_Sound = new Audio("sounds/loading_sound.mp3");
+
+var Focus_Planet_sound = new Audio("sounds/focus_planet.mp3");
+
+var Focus_Station_Sound = = new Audio("sounds/focus_station.mp3");
